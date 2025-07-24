525906127edfee04a2ca66bfde5117d8d3b1da26
grshaff <gshaffaat@gmail.com>
2025-07-25 03:55:52 +0700

    Fix: mainInfo page responsiveness

--- src/components/detail/mainInfo.tsx
+++ src/components/detail/mainInfo.tsx
@@ -15,9 +15,19 @@ interface Props {
 export default function MainInfo({ data, variant = 'page' }: Props) {
     const router = useRouter();
 
+    // link modal button to detail
     const handleDetailClick = () => {
       router.push(`/detail/${data.id}`);
     };
+
+    // Capitalize nameee
+    function capitalizeFirstLetter(str: string): string {
+      if (str.length === 0) {
+        return ""; 
+      }
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    const pokemonName: string = capitalizeFirstLetter(data.name);
 
   return (
     
@@ -28,15 +38,17 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
       <Container
         maxWidth="xl"
         sx={{
-            py: { xs: 4, sm: 6, md: 15 },
-            px: { xs: 2, sm: 4, md: 10 },
+            py: { xs: 4, sm: 6, md: 15, lg:15 },
+            px: { xs:0, sm: 0, md: '70px', lg:'14px' },
         }}
     >
         <Grid
           container
           spacing={4}
-          alignItems="start"
-          justifyContent="s"
+          alignItems={{xs:'center', md:'start'}}
+          justifyContent={{xs:'center', md:'start'}}
+          direction={{xs:'column', md:'row'}}
+          marginY={{xs:10, md:0}}
         >
           {/* Pokemon Image */}
           <Grid>
@@ -45,8 +57,8 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
               src={data.sprites.front_default ?? "/not-available.webp"}
               alt={data.name}
               sx={{
-                width: '100%',
-                minWidth: { xs: 220, sm: 280, md: 380 },
+                width: {xs: 220, sm:350, md:'100%'},
+                minWidth: { xs: 120, sm: 250, md: 280, lg:450 },
                 border: 2,
                 borderRadius: 2,
               }}
@@ -55,7 +67,7 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
 
           {/* Info Section */}
           <Grid>
-            <Stack spacing={0}>
+            <Stack spacing={0} >
               <Typography variant="body2" color="primary.light" fontWeight={700} fontSize={'24px'}>
                 #{data.id}
               </Typography>
@@ -70,26 +82,63 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
                     lg: '3.4rem',
                     xl: '4rem',
                   },
+                  marginBottom:'40px',
                   color: 'primary.main',
                 }}
               >
-                {data.name}
+                {pokemonName}
               </Typography>
 
               {/* Weight & Height */}
               <Stack direction="row" spacing={4}>
-                <Typography color="primary.light" fontWeight={700}>Weight:</Typography>
-                <Typography color="primary.light">{data.weight}</Typography>
-                <Typography color="primary.light" fontWeight={700}>Height:</Typography>
-                <Typography color="primary.light">{data.height}</Typography>
+                <Typography color="primary.main" fontWeight={700} fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.4rem',
+                   }}>Weight:</Typography>
+                <Typography color="primary.main" fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.3rem',
+                   }}>{data.weight}</Typography>
+                <Typography color="primary.main" fontWeight={700} paddingLeft={{xs:2, sm:15}} fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.3rem',
+                   }}>Height:</Typography>
+                <Typography color="primary.main"fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.3rem',
+                   }}>{data.height}</Typography>
               </Stack>
 
               {/* Abilities */}
-              <Stack direction="row" spacing={1} paddingY={2}>
-                <Typography color="primary.light" fontWeight={700}>Abilities:</Typography>
+              <Stack direction="row" spacing={3} paddingY={5}>
+                <Typography color="primary.main" fontWeight={700} fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.3rem',
+                   }}>Abilities:</Typography>
                 <Stack spacing={0.5}>
                   {data.abilities.map((ab, i) => (
-                    <Typography key={i} color="primary.light">
+                    <Typography key={i} color="primary.main" fontSize= {{
+                      xs: '1rem',
+                      sm: '1.2rem',
+                      md: '1rem',
+                      lg: '1.1rem',
+                      xl: '1.3rem',
+                     }}>
                       • {ab.ability.name}
                     </Typography>
                   ))}
@@ -97,8 +146,14 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
               </Stack>
 
               {/* Types */}
-              <Stack direction="row" spacing={2} alignItems="center">
-                <Typography color="primary.light" fontWeight={700}>Types:</Typography>
+              <Stack direction="row" spacing={4} alignItems="center">
+                <Typography color="primary.main" fontWeight={700}fontSize= {{
+                    xs: '1rem',
+                    sm: '1.2rem',
+                    md: '1rem',
+                    lg: '1.1rem',
+                    xl: '1.3rem',
+                   }}>Types:</Typography>
                 <Stack direction="row" spacing={2}>
                   {data.types.map((t, i) => (
                     <Link href={`/types/${t.type.name}`} key={i}>
@@ -164,7 +219,7 @@ export default function MainInfo({ data, variant = 'page' }: Props) {
                   textTransform: 'capitalize',
                 }}
               >
-                {data.name}
+                {pokemonName}
               </Typography>
 
               {/* Weight & Height */}
